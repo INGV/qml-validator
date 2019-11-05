520efe12da12865045774ab0fa90b0b2b2a36314
Valentino Lauciani <valentino.lauciani@ingv.it>
2019-11-05 14:17:07 +0100

    Add validation for 1.2 and 2.0

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,8 @@ ENV DEBIAN_FRONTEND=noninteractive
 ENV INITRD No
 ENV FAKE_CHROOT 1
 
-ENV QUAKEML_VERSION=quakeml-1.2.tgz
+ENV QUAKEML_VERSION_A=https://quake.ethz.ch/quakeml/Documents?action=AttachFile&do=get&target=quakeml-1.2.tgz
+ENV QUAKEML_VERSION_B=https://quake.ethz.ch/quakeml/QuakeML2.0?action=AttachFile&do=get&target=quakeml-2.0.tgz
 
 RUN apt-get update \
     && apt-get dist-upgrade -y --no-install-recommends \
@@ -16,9 +17,12 @@ RUN apt-get update \
 
 WORKDIR /opt
 
-RUN wget -O ${QUAKEML_VERSION} "https://quake.ethz.ch/quakeml/Documents?action=AttachFile&do=get&target=${QUAKEML_VERSION}" \
-    && tar -xvzf ${QUAKEML_VERSION} \
-    && rm -f ${QUAKEML_VERSION}
+RUN wget -O quakeml_a.tgz "${QUAKEML_VERSION_A}" \
+    && wget -O quakeml_b.tgz "${QUAKEML_VERSION_B}" \
+    && tar -xvzf quakeml_a.tgz \
+    && tar -xvzf quakeml_b.tgz \
+    && rm -f quakeml_a.tgz \
+    && rm -f quakeml_b.tgz
 
 COPY validator.php /opt
 
--- validator.php
+++ validator.php
@@ -2,10 +2,11 @@
 $FILE_QML_CACHE="input.xml";
 echo "\n";
 if (file_exists($FILE_QML_CACHE)) {
-        echo "Validating $FILE_QML_CACHE \n";
         libxml_use_internal_errors(true);
         $xml = new DOMDocument();
         $xml->load("$FILE_QML_CACHE");
+
+        echo "Validating $FILE_QML_CACHE with schema 1.2 \n";
         if ( !$xml->schemaValidate('./1.2/QuakeML-1.2.xsd') ) {
 
                 $errors = libxml_get_errors();
@@ -18,6 +19,22 @@ if (file_exists($FILE_QML_CACHE)) {
         else {
                 echo " QuakeML is valid. \n";
         }
+	echo "\n";
+
+        echo "Validating $FILE_QML_CACHE with schema 2.0 \n";
+        if ( !$xml->schemaValidate('./xsd/QuakeML-2.0.xsd') ) {
+
+                $errors = libxml_get_errors();
+                foreach ($errors as $error) {
+                        $validation_error = " XML error:\n  MESSAGE=$error->message  LEVEL=[$error->level]\n  CODE=$error->code\n  FILE=$error->file\n  LINE=$error->line \n\n";
+                }
+                echo " QuakeML is not valid: \n$validation_error";
+                libxml_clear_errors();
+        }
+        else {
+                echo " QuakeML is valid. \n";
+        }
+
         libxml_use_internal_errors(false);
 } else {
 	echo "The file $FILE_QML_CACHE doesn't exist";
